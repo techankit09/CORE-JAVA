0765612f3e03bbbe3423d2c7ec1df2aa64b8e7aa
ankitrawat-cloud <ankitrawat0988@gmail.com>
2025-08-20 18:27:46 +0530

    done

--- polymorphism/TestShapeByArrays.java
+++ polymorphism/TestShapeByArrays.java
@@ -6,25 +6,16 @@ public class TestShapeByArrays {
 
 		Shape[] s = new Shape[3];
 
-		s[0] = new Circle();
-		s[1] = new Rectangle();
-		s[2] = new Triangle();
-
-		Circle c = (Circle) s[0];
-		c.setRadius(2);
-
-		Rectangle r = (Rectangle) s[1];
-		r.setLenght(5);
-		r.setWidht(10);
-
-		Triangle t = (Triangle) s[2];
-		t.setBase(5);
-		t.setHight(10);
+		s [0] = new Circle(5);
+		s[1]  = new Rectangle(9,8);
+		s[2]  = new Triangle(6,8);
+		
+		
 		
 		for (int i = 0; i < s.length; i++) {
-			System.out.println("area: " + s[i].area());
+			System.out.println("Area :"+ s[i].area());
 		}
-
 	}
+		}
+        
 
-}
