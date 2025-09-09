bb806145b086ac37686d04b974378fc9cdf7895a
ankitrawat-cloud <ankitrawat0988@gmail.com>
2025-09-09 18:10:24 +0530

    Done

--- Java-IO/src/com/rays/io/ReadFromKeyBoard.java
+++ Java-IO/src/com/rays/io/ReadFromKeyBoard.java
@@ -1,31 +1,32 @@
 package com.rays.io;
 
-	import java.io.BufferedReader;
-	import java.io.FileWriter;
-	import java.io.IOException;
-	import java.io.InputStreamReader;
-	import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
-	public class ReadFromKeyBoard {
+public class ReadFromKeyBoard {
 
-		public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-			PrintWriter out = new PrintWriter(new FileWriter("D://IO//KeyBoard.txt"));
+		PrintWriter out = new PrintWriter(new FileWriter("D://IO//KeyBoard.txt"));
 
-			InputStreamReader kb = new InputStreamReader(System.in);
-			System.out.println("type somthing...");
-			BufferedReader in = new BufferedReader(kb);
+		InputStreamReader kb = new InputStreamReader(System.in);
+		System.out.println("type somthing...");
+		BufferedReader in = new BufferedReader(kb);
 
-			String line = in.readLine();
+		String line = in.readLine();
 
-			while (!(line.equals("quit"))) {
-				out.write(line+"\n");
-				line = in.readLine();
-			}
-			out.close();
-
+		while (!(line.equals("quit"))) {
+			out.write(line+"\n");
+			line = in.readLine();
 		}
+
+		out.close();
+		in.close();
+		kb.close();
 
 	}
 
-
+}
